44163c5ae451c659073dcfd733e67b708e39a5c7
Jearamo <jeh.arancibia@duocuc.cl>
2024-08-24 02:32:43 -0400

    Login funcional

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -6,16 +8,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  // Mostrar contraseña
   showPassword: boolean = false;
+  // verificar inicio 
+  correo: string = '';
+  contrasena: string = '';
 
-  user:string = "Jehison";
-  pass:string = "admin";
+  validEmail: string = 'Jehison';
+  validPassword: string = 'admin123';
 
-  constructor() { }
+  constructor(private router: Router, private alertController: AlertController) { }
+  // MOSTRAR CONTRASEÑA
   togglePasswordVisibility() {
     this.showPassword = !this.showPassword;
     const passwordInput = document.querySelector('ion-input[type="password"]') as HTMLIonInputElement;
     passwordInput.type = this.showPassword ? 'text' : 'password';
+  }
+  // IF PARA INICIO DE SESIÓN
+  async login() {
+    if (this.correo === this.validEmail && this.contrasena === this.validPassword) {
+      // VÁLIDO
+      this.router.navigate(['/tabs/tab1']); // DIRIGE A LA PRINCIPAL
+    } else {
+      // INVÁLIDO
+      const alert = await this.alertController.create({
+        header: 'Inicio de sesión fallido',
+        message: 'Correo o contraseña inválidos. Por favor, intente de nuevo.',
+        buttons: ['OK']
+      });
+      await alert.present();
+    }
   }
 
   ngOnInit() {
