ef2e3f8a4c705c41efe172395455d15d73be57b9
Jearamo <jeh.arancibia@duocuc.cl>
2024-09-02 23:31:19 -0400

    final

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tab2',
@@ -6,7 +8,27 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  titulo: string = '';
+  contenido: string = '';
 
-  constructor() {}
+  constructor(private alertController: AlertController, private router: Router) {}
 
+  async publicar() {
+    const tituloTrimmed = this.titulo.trim();
+    const contenidoTrimmed = this.contenido.trim();
+
+    if (!tituloTrimmed || !contenidoTrimmed) {
+      const alert = await this.alertController.create({
+        header: 'Error',
+        message: 'Por favor, rellene todos los campos.',
+        buttons: ['OK']
+      });
+
+      await alert.present();
+    } else {
+      console.log('Título:', tituloTrimmed);
+      console.log('Contenido:', contenidoTrimmed);
+      this.router.navigate(['/exito']);
+    }
+  }
 }
